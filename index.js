6139790aaa6b6bb88823279e29bd4de089df0c1f
JVsilveira <joaovitor.santossilveira@gmail.com>
2025-02-12 17:22:05 -0300

    finalização da parte de design da página. Falta somente ajustar @media

--- index.js
+++ index.js
@@ -38,14 +38,14 @@ function resetAutoChange() {
 startAutoChange()
 
 function toggleMenu() {
-  var menu = document.getElementById("dropdownMenu");
-  var hamburger = document.getElementById("hamburgerMenu");
-  
+  var menu = document.getElementById("dropdownMenu")
+  var hamburger = document.getElementById("hamburgerMenu")
+
   if (menu.style.display === "block") {
-    menu.style.display = "none";  
-    hamburger.classList.remove("active"); 
+    menu.style.display = "none"
+    hamburger.classList.remove("active")
   } else {
-    menu.style.display = "block";  
-    hamburger.classList.add("active"); 
+    menu.style.display = "block"
+    hamburger.classList.add("active")
   }
 }
